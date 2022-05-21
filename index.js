e0001a4e646bfc42e9a19860d049b9f0d7effd46
Yuji Sasaki <jyu0414@gmail.com>
2022-05-21 16:13:09 +0900

    UPDATE

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: 24.8054, lng: 125.2811 },
+    zoom: 12,
   });
 
   // const marker1 = new google.maps.Marker({
